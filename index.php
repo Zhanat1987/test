f561d00a0d1b9c7942b2fb5d26b5edff738148ae
admin <iskakov_zhanat@mail.ru>
2014-03-01 18:53:17 +0600

    Mongo class

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
+//header('Content-Type: text/html; charset=utf-8');
 ?>
-<h1>heroku test merge</h1>
-<script type="text/javascript" src="todo.js"></script>
+<!--<h1>heroku test merge</h1>-->
+<!--<script type="text/javascript" src="todo.js"></script>-->
 <?php
-echo '<p>Текущие дата и время - <b>' . date('m.d.Y H:i:s') . '</b></p>';
+//echo '<p>Текущие дата и время - <b>' . date('m.d.Y H:i:s') . '</b></p>';
 phpinfo();
 ?>
