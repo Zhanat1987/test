29f3420c54ec544c010860380993583d1ce5e397
admin <iskakov_zhanat@mail.ru>
2014-03-01 12:53:48 +0600

    include outer js file

--- index.php
+++ index.php
@@ -6,3 +6,4 @@ header('Content-Type: text/html; charset=utf-8');
 echo '<p>Текущие дата и время - <b>' . date('m.d.Y H:i:s') . '</b></p>';
 ?>
 <?php phpinfo(); ?>
+<script type="text/javascript" src="todo.js"></script>
--- todo.js
+++ todo.js
@@ -2,3 +2,4 @@
  * Created by admin on 28.02.14.
  */
 // TODO: write code
+alert(1);
